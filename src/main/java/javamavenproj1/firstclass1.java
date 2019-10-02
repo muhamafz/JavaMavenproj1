cc91a6feff701ade0161d38b1c453d32323f4d7f
Afzal M <mafzal2005@gmail.com>
2019-10-02 14:55:00 -0400

    uploading the repo to the github

---
+++ src/main/java/javamavenproj1/firstclass1.java
@@ -0,0 +1,11 @@
+package javamavenproj1;
+
+public class firstclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
